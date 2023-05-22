27c89630ccb393a431fb5fbf86af411f6f3270e5
Diogo Bastos <diogo.bastos@hmhco.com>
2023-05-21 21:28:20 -0400

    Escalas

--- src/views/Escala/index.tsx
+++ src/views/Escala/index.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -8,16 +8,17 @@ import {
   Dimensions,
 } from 'react-native';
 import Carousel, {Pagination} from 'react-native-snap-carousel';
-// import {
-//   heightPercentageToDP as hp,
-//   widthPercentageToDP as wp,
-// } from 'react-native-responsive-screen';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
 
-import {FALLBACK, MINISTERIO} from './data/Escala';
+import {MINISTERIO} from './data/Escala';
 import {ContainerPage} from '../../components/ContainerPage';
 import {FONT_GILLSANS, WAIKAWAGREY, WHITE} from '../../styles/styles';
 import CarouselCardItem from './CarouselCardItem';
-import {createSetFromArray} from '../../utils/utils';
+import {BACKEND_URL, createSetFromArray} from '../../utils/utils';
+import {Aguarde} from '../../components/Aguarde';
 
 export interface Equipe {
   nome: string;
@@ -50,11 +51,39 @@ export const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7);
 
 export const Escala = () => {
   const isCarousel = useRef(null);
+  const [isLoading, setLoading] = useState(true);
   const [menuIndex, setMenuIndex] = useState(0);
   const [cardIndex, setCardIndex] = useState(0);
-  const [activeMinisterio, setActiveMinisterio] = useState<string>(
-    FALLBACK[0].ministerio,
-  );
+  const [escalaList, setEscalaList] = useState<Escala[]>([]);
+  const [activeMinisterio, setActiveMinisterio] = useState<string>();
+
+  useEffect(() => {
+    fetch(`${BACKEND_URL}/compromissos`)
+      .then((response) => response.json())
+      .then((json) => {
+        setEscalaList(
+          json.sort((a: Escala, b: Escala) => {
+            if (a.ministerio < b.ministerio) {
+              return -1;
+            }
+            if (a.ministerio > b.ministerio) {
+              return 1;
+            }
+            return 0;
+          }),
+        );
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  useEffect(() => {
+    if (escalaList.length > 0) {
+      setActiveMinisterio(escalaList[0].ministerio);
+    }
+  }, [escalaList]);
 
   const escalaMenuItem = ({item, index}: MenuEscalaItem) => {
     const title = (MINISTERIO as any)[item.ministerio];
@@ -75,31 +104,22 @@ export const Escala = () => {
     );
   };
 
-  const data = FALLBACK.filter(
-    (escala) => new Date(escala.inicio) >= new Date(),
-  ).sort((a: Escala, b: Escala) => {
-    if (new Date(a.inicio) < new Date(b.inicio)) {
-      return -1;
-    }
-    if (new Date(a.inicio) > new Date(b.inicio)) {
-      return 1;
-    }
-    return 0;
-  });
-
-  const menuData = [...new Map(data.map((m) => [m.ministerio, m])).values()];
-  let cardsData = data
-    .filter((escala) => escala.ministerio === activeMinisterio)
+  const data = escalaList
+    .filter((escala) => new Date(escala.inicio) >= new Date())
     .sort((a: Escala, b: Escala) => {
-      if (a.ministerio < b.ministerio) {
+      if (new Date(a.inicio) < new Date(b.inicio)) {
         return -1;
       }
-      if (a.ministerio > b.ministerio) {
+      if (new Date(a.inicio) > new Date(b.inicio)) {
         return 1;
       }
       return 0;
     });
 
+  const menuData = [...new Map(data.map((m) => [m.ministerio, m])).values()];
+  let cardsData = data.filter(
+    (escala) => escala.ministerio === activeMinisterio,
+  );
   const resultMap = createSetFromArray(cardsData);
   const escalasByDay: EscalaByDay[] = Array.from(
     resultMap,
@@ -112,39 +132,45 @@ export const Escala = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
       <ContainerPage titulo={'ESCALAS'}>
-        <View style={styles.container}>
-          <Carousel
-            firstItem={menuIndex}
-            layout="default"
-            layoutCardOffset={9}
-            data={menuData}
-            renderItem={escalaMenuItem}
-            sliderWidth={400}
-            itemWidth={160}
-            useScrollView={true}
-            onSnapToItem={(index) => setMenuIndex(index)}
-          />
-          <Carousel
-            layout="default"
-            layoutCardOffset={9}
-            ref={isCarousel}
-            data={escalasByDay}
-            renderItem={CarouselCardItem}
-            sliderWidth={SLIDER_WIDTH}
-            itemWidth={ITEM_WIDTH}
-            onSnapToItem={(i) => setCardIndex(i)}
-            useScrollView={true}
-          />
-          <Pagination
-            dotsLength={escalasByDay.length}
-            activeDotIndex={cardIndex}
-            carouselRef={isCarousel}
-            dotStyle={styles.dotStyle}
-            inactiveDotOpacity={0.4}
-            inactiveDotScale={0.6}
-            tappableDots={true}
-          />
-        </View>
+        {isLoading ? (
+          <View style={styles.aguarde}>
+            <Aguarde />
+          </View>
+        ) : (
+          <View style={styles.container}>
+            <Carousel
+              firstItem={menuIndex}
+              layout="default"
+              layoutCardOffset={9}
+              data={menuData}
+              renderItem={escalaMenuItem}
+              sliderWidth={400}
+              itemWidth={160}
+              useScrollView={true}
+              onSnapToItem={(index) => setMenuIndex(index)}
+            />
+            <Carousel
+              layout="default"
+              layoutCardOffset={9}
+              ref={isCarousel}
+              data={escalasByDay}
+              renderItem={CarouselCardItem}
+              sliderWidth={SLIDER_WIDTH}
+              itemWidth={ITEM_WIDTH}
+              onSnapToItem={(i) => setCardIndex(i)}
+              useScrollView={true}
+            />
+            <Pagination
+              dotsLength={escalasByDay.length}
+              activeDotIndex={cardIndex}
+              carouselRef={isCarousel}
+              dotStyle={styles.dotStyle}
+              inactiveDotOpacity={0.4}
+              inactiveDotScale={0.6}
+              tappableDots={true}
+            />
+          </View>
+        )}
       </ContainerPage>
     </SafeAreaView>
   );
@@ -186,5 +212,10 @@ const styles = StyleSheet.create({
     marginTop: -9.9,
     borderRadius: 5,
     marginHorizontal: 0,
+  },
+  aguarde: {
+    alignItems: 'center',
+    marginBottom: hp('4%'),
+    marginTop: hp('15%'),
   },
 });
